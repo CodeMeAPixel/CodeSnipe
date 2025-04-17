30b3b0fc33838de61ea9d3614828d791171376b2
TheRealToxicDev <toxic.dev09@gmail.com>
2025-04-16 19:24:19 -0600

    update(seo): fix stuff

--- constants/seo.ts
+++ constants/seo.ts
@@ -2,7 +2,7 @@ export const SEO_TITLE: string =
   "CodeSnipe";
 export const SEO_DESCRIPTION: string =
   "Create beautiful code snippets with CodeSnipe. Choose from a range of themes, hide or show the background, toggle between a dark and light window, change font styles and more. Customize it your way.";
-export const SEO_IMAGE: string = "/images/cover.png";
+export const SEO_IMAGE: string = "/cover.png";
 export const SEO_KEYWORDS: string = "create, code, screenshots, beautiful, generator, terminal , window";
 
 export const SEO_TWITTER_CREATOR: string = "@codemeapixel";
